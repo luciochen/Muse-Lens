3898d709ca43801681ae3f79eb00e420bef0abc1
luciochen <luciochen79@gmail.com>
2025-11-05 19:45:57 -0800

    Initial Commit

---
+++ Muse Lens/Muse_LensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muse_LensApp.swift
+//  Muse Lens
+//
+//  Created by Lucio Chen on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Muse_LensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
